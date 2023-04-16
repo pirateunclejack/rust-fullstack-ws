145ec64b87eab81d7f9f77b6a617d2a914408855
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-04-17 00:49:49 +0800

    Finish use database in web project 2

--- webservice/src/handlers.rs
+++ webservice/src/handlers.rs
@@ -1,3 +1,4 @@
+use super::db_access::*;
 use super::state::AppState;
 use actix_web::{web, HttpResponse};
 
@@ -11,75 +12,33 @@ pub async fn health_check_handler(app_state: web::Data<AppState>) -> HttpRespons
 }
 
 use super::models::Course;
-use chrono::Utc;
+// use chrono::Utc;
 
 pub async fn new_course(
     new_course: web::Json<Course>,
     app_state: web::Data<AppState>,
 ) -> HttpResponse {
-    // println!("Received new course");
-    // let course_count = app_state
-    //     .courses
-    //     .lock()
-    //     .unwrap()
-    //     .clone()
-    //     .into_iter()
-    //     .filter(|course| course.teacher_id == new_course.teacher_id)
-    //     .collect::<Vec<Course>>()
-    //     .len();
-    // let new_course = Course {
-    //     teacher_id: new_course.teacher_id,
-    //     id: Some(course_count + 1),
-    //     name: new_course.name.clone(),
-    //     time: Some(Utc::now().naive_utc()),
-    // };
-    // app_state.courses.lock().unwrap().push(new_course);
-    HttpResponse::Ok().json("Course added")
+    let course = post_new_course_db(&app_state.db, new_course.into()).await;
+    HttpResponse::Ok().json(course)
 }
 
 pub async fn get_courses_for_teacher(
     app_state: web::Data<AppState>,
     params: web::Path<(usize,)>,
 ) -> HttpResponse {
-    // let teacher_id: usize = params.0;
-
-    // let filtered_courses = app_state
-    //     .courses
-    //     .lock()
-    //     .unwrap()
-    //     .clone()
-    //     .into_iter()
-    //     .filter(|course| course.teacher_id == teacher_id)
-    //     .collect::<Vec<Course>>();
-
-    // if filtered_courses.len() > 0 {
-    //     HttpResponse::Ok().json(filtered_courses)
-    // } else {
-    //     HttpResponse::Ok().json("No courses found for teacher".to_string())
-    // }
-    HttpResponse::Ok().json("Course added")
+    let teacher_id = i32::try_from(params.0).unwrap();
+    let courses = get_courses_for_teacher_db(&app_state.db, teacher_id).await;
+    HttpResponse::Ok().json(courses)
 }
 
 pub async fn get_course_detail(
     app_state: web::Data<AppState>,
     params: web::Path<(usize, usize)>,
 ) -> HttpResponse {
-    // let (teacher_id, course_id) = params.0;
-    // let selected_course = app_state
-    //     .courses
-    //     .lock()
-    //     .unwrap()
-    //     .clone()
-    //     .into_iter()
-    //     .find(|x| x.teacher_id == teacher_id && x.id == Some(course_id))
-    //     .ok_or("Course not found");
-
-    // if let Ok(course) = selected_course {
-    //     HttpResponse::Ok().json(course)
-    // } else {
-    //     HttpResponse::Ok().json("Course not found".to_string())
-    // }
-    HttpResponse::Ok().json("Course added")
+    let teacher_id = i32::try_from(params.0).unwrap();
+    let course_id = i32::try_from(params.1).unwrap();
+    let course = get_course_details_db(&app_state.db, teacher_id, course_id).await;
+    HttpResponse::Ok().json(course)
 }
 
 #[cfg(test)]
@@ -100,7 +59,7 @@ mod tests {
         let course = web::Json(Course {
             teacher_id: 1,
             name: "Test course".into(),
-            id: None,
+            id: Some(3),
             time: None,
         });
         let app_state: web::Data<AppState> = web::Data::new(AppState {
